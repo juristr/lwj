87462235e6bb14e58191a601e29a15dd15784a37
Juri <juri.strumpflohner@gmail.com>
2022-04-21 22:40:03 +0200

    add jokes-admin

---
+++ apps/jokes-admin/src/app/app.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FeatureJokesList } from '@lwj/feature-jokes-list';
+
+export function App() {
+  return <FeatureJokesList />;
+}
+
+export default App;
