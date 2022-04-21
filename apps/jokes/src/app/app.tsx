30d6461b6e34ed40c7ec2ed7ed85eb2a6547e644
Juri <juri.strumpflohner@gmail.com>
2022-04-21 22:36:55 +0200

    add jokes list to app

--- apps/jokes/src/app/app.tsx
+++ apps/jokes/src/app/app.tsx
@@ -1,13 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-// import NxWelcome from './nx-welcome';
 import { FeatureJokeOfTheDay } from '@lwj/feature-joke-of-the-day';
+
+import { Route, Link, Routes } from 'react-router-dom';
+import { FeatureJokesList } from '@lwj/feature-jokes-list';
 
 export function App() {
   return (
     <>
-      {/* <NxWelcome title="jokes" /> */}
-      <FeatureJokeOfTheDay />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Joke of the Day</Link>
+          </li>
+          <li>
+            <Link to="/list">List of jokes</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<FeatureJokeOfTheDay />} />
+        <Route path="/list" element={<FeatureJokesList />} />
+      </Routes>
     </>
   );
 }
